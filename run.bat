dcfb071ab2ff90baa124050a0653546aea879f46
ysabynin <evgensabyka@mail.ru>
2016-10-24 23:45:56 +0300

    Added tests, 3 target grammars

---
+++ run.bat
@@ -0,0 +1,7 @@
+java -jar cyk.jar grammar.txt test.txt
+java -jar cyk.jar grammar1.txt test.txt
+java -jar cyk.jar grammar2.txt test.txt
+java -jar cyk.jar grammar4.txt test4.txt
+java -jar cyk.jar grammar5.txt test5.txt
+java -jar cyk.jar grammar6.txt test6.txt
+pause
